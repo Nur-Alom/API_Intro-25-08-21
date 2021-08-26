3c30a39353d9ec4102603f5128369891d47f9e85
Nur-Alom <alamn7150@gmail.com>
2021-08-26 14:37:54 +0600

    last commit

---
+++ js/album.js
@@ -0,0 +1,20 @@
+
+function loadAlbum() {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(res => res.json())
+        .then(data => displayAlbums(data))
+};
+loadAlbum();
+
+
+function displayAlbums(albums) {
+    const albumContainer = document.getElementById('albums');
+    console.log(albums);
+    for (const album of albums) {
+        const p = document.createElement('p');
+        p.style.textAlign = 'center';
+        p.innerText = album.title;
+        albumContainer.appendChild(p);
+    }
+}
+
--- js/api.js
+++ js/api.js
@@ -4,3 +4,29 @@ function loadData() {
         .then(response => response.json())
         .then(data => console.log(data))
 };
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => displayUsers(data))
+};
+
+function loadPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data))
+};
+// loadPost();
+
+function displayUsers(data) {
+    const ul = document.getElementById('users');
+
+    for (const user of data) {
+        // console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = `name: ${user.name},
+        Email: ${user.email}`;
+        ul.appendChild(li);
+    }
+}
+
